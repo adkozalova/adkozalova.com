1bee021893c34d56ac3bcb1c5073d51116459b8f
Igor Lubenets <ilubenets@gmail.com>
2020-06-12 22:59:59 +0300

    avoid hiding menu on projects click

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,25 +10,27 @@ export default class Header extends Component {
     this.state = {isMenuOpen: false};
   }
 
-  onMenuClick = () => {
-    this.setState({isMenuOpen: !this.state.isMenuOpen})
+  onMenuClick = (e) => {
+    if (e.target.className === 'not-hide-on-click') {
+      return;
+    }
+    this.setState({isMenuOpen: !this.state.isMenuOpen});
     document.querySelector('body').style.overflowY = !this.state.isMenuOpen ? 'hidden' : 'auto';
   }
 
   render() {
     return (
-        <header className={this.state.isMenuOpen ? "header menu_open" : "header"}>
+        <header className={this.state.isMenuOpen ? "header menu_open" : "header"} onClick={this.onMenuClick.bind(this)}>
           <div className="container-fluid">
             <div className="row">
               <div className="col-lg-12 col-12">
                 <nav className="navbar">
-                  <Link to="/" className="navbar-brand logo" ><span className="logo_text">Adkozalova</span></Link>
+                  <Link to="/" className="navbar-brand logo"><span className="logo_text">Adkozalova</span></Link>
                   <button
                       className="navbar-toggler hamburger"
                       type="button"
                       data-toggle="collapse"
                       data-target="#header_menu"
-                      onClick={this.onMenuClick}
                   >
                     <span className="m_menu">Menu</span>
                     <span className="m_close">Close</span>
@@ -41,19 +43,19 @@ export default class Header extends Component {
                   <div className="opnen_menu">
                     <div className="header_main_menu">
                       <ul className="menu_item">
-                        <li><Link to="/" onClick={this.onMenuClick}>Home</Link></li>
+                        <li><Link to="/">Home</Link></li>
                         <li className="submenu">
-                          <Link to="/">Projects</Link>
+                          <Link className="not-hide-on-click" to="/">Projects</Link>
                           <ul className="submenu_item">
-                            <li><Link to="/black-and-white" onClick={this.onMenuClick}>Black & White</Link></li>
-                            <li><Link to="/jazzman" onClick={this.onMenuClick}>Jazzman</Link></li>
-                            <li><Link to="/watercolor" onClick={this.onMenuClick}>Watercolor</Link></li>
-                            <li><Link to="/sketch" onClick={this.onMenuClick}>Sketch</Link></li>
+                            <li><Link to="/black-and-white">Black & White</Link></li>
+                            <li><Link to="/jazzman">Jazzman</Link></li>
+                            <li><Link to="/watercolor">Watercolor</Link></li>
+                            <li><Link to="/sketch">Sketch</Link></li>
                           </ul>
                         </li>
-                        <li><Link to="/rewards" onClick={this.onMenuClick}>Rewards</Link></li>
-                        <li><Link to="/about" onClick={this.onMenuClick}>About me</Link></li>
-                        <li><Link to="/contacts" onClick={this.onMenuClick}>Contacts</Link></li>
+                        <li><Link to="/rewards">Rewards</Link></li>
+                        <li><Link to="/about">About me</Link></li>
+                        <li><Link to="/contacts">Contacts</Link></li>
                       </ul>
                     </div>
                   </div>
